b49d012d8c35b7860461b8fdd4f619e55eeec5ad
DragonRF <duonglong410@gmail.com>
2023-05-29 11:11:43 +0700

    add gitignore in BE

--- BE/dist/src/enitity/order.d.ts
+++
@@ -1,9 +0,0 @@
-import { OrderDetail } from './orderDetail';
-import { User } from './user';
-export declare class Order {
-    id: number;
-    status: boolean;
-    totalMoney: number;
-    orderDetails: OrderDetail[];
-    user: User;
-}
--- FE/my-react-app/src/pages/user/Login.js
+++ FE/my-react-app/src/pages/user/Login.js
@@ -14,7 +14,6 @@ export function Login() {
         });
     }
 
-
     return (
         <>
             <center>
